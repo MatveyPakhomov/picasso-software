86aa4ecf2ad6b7cad5490bcb38f6026c8382d14b
Matvey Pakhomov <matwick12@gmail.com>
2022-12-15 14:50:11 +0400

    remote

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [values, setValues] = useState([]);
+  console.log(values);
+
+  const getCompanies = async (name) => {
+    const url = `https://autocomplete.clearbit.com/v1/companies/suggest?query=:${name}`;
+    let response = await fetch(url);
+    let json = await response.json();
+    setValues(json);
+  };
+
+  const handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    getCompanies(value);
+  };
+
+  const renderAutocomplete = () => {
+    return values.map((item) => {
+      return (
+        <li className="autocomplete__item">
+          <img src={item.logo} alt="" className="autocomplete__image" />
+          <span className="autocomplete__span">
+            <p className="autocomplete__company">{item.name}</p>
+            <p className="autocomplete__domain">{item.domain}</p>
+          </span>
+        </li>
+      );
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className="form" autoComplete="off">
+          <p className="input__label">Компания</p>
+          <input
+            type="text"
+            name="name"
+            className="input"
+            onChange={handleChange}
+          />
+          {values.length ? (
+            <ul className="autocomplete">{renderAutocomplete()}</ul>
+          ) : null}
+          {renderAutocomplete}
+        </form>
       </header>
     </div>
   );
